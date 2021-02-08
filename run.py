38605ae9d7f24539bfdc1fd4e7c29106fb6fd340
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-02-09 00:26:08 +0200

    tweak the setup

--- run.py
+++ run.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == "main":
+    app.debug(True)
 
 @app.route('/')
 def home():
-    return render_template('index.html', test_var="Coming from the server")
+    return render_template('index.html', test_var="Coming from the server you get it?")
 
 
-if __name__ == "main":
-    app.debug(True)
+
