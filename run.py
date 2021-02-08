6807d48a097a6f9e10e8cf05145c18032f5afbcf
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-02-09 00:21:38 +0200

    print stuff from the server

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Home"
+    return render_template('index.html', test_var="Coming from the server")
 
 
 if __name__ == "main":
